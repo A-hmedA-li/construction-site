27c615dccab2dede34d6e035a3e263114d3d69ca
A-hmedA-li <76922181+A-hmedA-li@users.noreply.github.com>
2025-12-12 21:23:51 +0300

    Fix input signip

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 import { Eye, EyeOff } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
-
+import { Input } from "@/components/ui/input"
 import { useLanguage } from "@/components/providers/language-provider"
 import { translations } from "@/lib/styles"
 
